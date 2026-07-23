a63bca6146d47185ed8617a3320f3daaf8d1d1c4
韩子屹 <ziyi_han@foxmail.com>
2026-07-23 09:47:18 +0800

    Initial Commit

---
+++ 性同意/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  性同意
+//
+//  Created by Li&Han on 2026/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
